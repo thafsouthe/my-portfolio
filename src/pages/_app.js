2e7d352a36370708cced040f478fdbbeee806f8e
pc <thafsouthehassani@gmail.com>
2025-08-01 17:28:34 +0100

    forth commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
