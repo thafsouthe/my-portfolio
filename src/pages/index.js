8c4cfd09b34728ddd81976b07b613558b767dbab
pc <thafsouthehassani@gmail.com>
2025-08-01 17:53:26 +0100

    seven commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello, I&aposm HASSANI Thafsouthe</h1>
+      <h1>Hello, I&lsquo;m HASSANI Thafsouthe</h1>
       <Image
         src="/profile.jpg"
         alt="Profile"
